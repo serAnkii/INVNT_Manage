99c5018dc31cd29dddf0e622e1c188b383b187d1
serAnkii <recruitankitnegi2023@gmail.com>
2023-09-28 11:23:03 +0530

    feat:init backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,28 @@
 const express = require("express");
 
-const app = express;
+const app = express();
 
 const port = 5001;
 
-const moralis = require('moralis');
+const moralis = require('moralis').default;
+
+const cors = require('cors');
+const { default: Moralis } = require("moralis");
+
+require("dotenv").config({path:".env"});
+
+
+app.use(cors());
+app .use(express.json());
+
+const key = process.env.key;
+
+//making the endpoints 
+
+
+
+Moralis.start({apiKey:key}).then(()=>{
+    app.listen(port,()=>{
+        console.log(`listening sir`)
+    })
+})
